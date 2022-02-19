bc32c9b8ac75a82620cad145e2205569cec5e035
clickingbuttons <clickingbuttons@pm.me>
2022-02-19 12:07:54 -0500

    working axes selection

--- jeditrader/axes.c
+++ jeditrader/axes.c
@@ -37,14 +37,12 @@ float vertices[14][6] = {
 };
 
 // XY selection
-bool sel_started = false;
-bool sel_ended = false;
+bool selecting = false;
 vec2 sel_start = { 0 };
 vec2 sel_end = { 0 };
 
 void sel_reset() {
-  sel_started = false;
-  sel_ended = false;
+  selecting = false;
   sel_start.x = 0;
   sel_start.y = 0;
   sel_end.x = 0;
@@ -52,34 +50,34 @@ void sel_reset() {
 }
 
 void sel_write_vertices() {
-  float min_x = sel_started ? min(sel_start.x, sel_end.x) : 0;
-  float min_y = sel_started ? min(sel_start.y, sel_end.y) : 0;
-  float max_x = sel_started ? max(sel_start.x, sel_end.x) : 0;
-  float max_y = sel_started ? max(sel_start.y, sel_end.y) : 0;
+  float min_x = selecting ? min(sel_start.x, sel_end.x) : 0;
+  float min_y = selecting ? min(sel_start.y, sel_end.y) : 0;
+  float max_x = selecting ? max(sel_start.x, sel_end.x) : 0;
+  float max_y = selecting ? max(sel_start.y, sel_end.y) : 0;
 
-  vertices[6][1] = min_x;
-  vertices[6][2] = -min_y;
+  vertices[6][0] = min_x;
+  vertices[6][1] = -min_y;
 
-  vertices[7][1] = min_x;
-  vertices[7][2] = -max_y;
+  vertices[7][0] = min_x;
+  vertices[7][1] = -max_y;
 
-  vertices[7][1] = min_x;
-  vertices[7][2] = -max_y;
+  vertices[8][0] = min_x;
+  vertices[8][1] = -max_y;
 
-  vertices[7][1] = max_x;
-  vertices[7][2] = -max_y;
+  vertices[9][0] = max_x;
+  vertices[9][1] = -max_y;
 
-  vertices[7][1] = max_x;
-  vertices[7][2] = -max_y;
+  vertices[10][0] = max_x;
+  vertices[10][1] = -max_y;
 
-  vertices[7][1] = max_x;
-  vertices[7][2] = -min_y;
+  vertices[11][0] = max_x;
+  vertices[11][1] = -min_y;
 
-  vertices[7][1] = max_x;
-  vertices[7][2] = -min_y;
+  vertices[12][0] = max_x;
+  vertices[12][1] = -min_y;
 
-  vertices[7][1] = min_x;
-  vertices[7][2] = -min_y;
+  vertices[13][0] = min_x;
+  vertices[13][1] = -min_y;
 }
 
 void _print_programme_info_log(GLuint programme) {
@@ -159,24 +157,16 @@ void axes_render_frame(mat4 g_world) {
   glDrawArrays(GL_LINES, 0, sizeof(vertices)/sizeof(float));
 }
 
-vec2 line_plane_collision(vec3 plane_norm, vec3 plane_pnt, vec3 ray_dir, vec3 ray_pnt) {
-  vec2 res = { 0 };
+vec3 line_plane_collision(vec3 plane_norm, vec3 plane_pnt, vec3 ray_dir, vec3 ray_pnt) {
+  vec3 diff = vec3_sub(ray_pnt, plane_pnt);
+  float prod1 = vec3_dot(diff, plane_norm);
+  float prod2 = vec3_dot(ray_dir, plane_norm);
+  float prod3 = prod1 / prod2;
 
-  float ndotu = vec3_dot(plane_norm, ray_dir);
-  if (ndotu == 0.0) {
-    return res;
-  }
-
-  vec3 w = vec3_sub(ray_pnt, plane_pnt);
-  float si = -vec3_dot(plane_norm, w) / ndotu;
-  vec3 tao = vec3_add(vec3_add(w, vec3_multf(ray_dir, si)), ray_pnt);
-
-  res.x = tao.x;
-  res.y = tao.y;
-  return res;
+  return vec3_sub(ray_pnt, vec3_multf(ray_dir, prod3));
 }
 
-vec2 world_coords_xy(Window* window, struct Cam* cam) {
+vec3 world_coords_xyz(Window* window, struct Cam* cam) {
   // https://antongerdelan.net/opengl/raycasting.html
   // Step 0: 2d Viewport Coordinates
   double xoff, yoff;
@@ -184,46 +174,39 @@ vec2 world_coords_xy(Window* window, struct Cam* cam) {
   // Step 1: 3d Normalised Device Coordinates
   double x = (2.0 * xoff) / (float)window->width - 1.0;
   double y = 1.0 - (2.0 * yoff) / (float)window->height;
-  printf("x y %f %f\n", x, y);
   // Step 2: 4d Homogeneous Clip Coordinates
   vec4 ray_clip = (vec4) { x, y, -1.0, 1.0 };
   // Step 3: 4d Eye (Camera) Coordinates
-  printf("proj\n");
-  mat4_print(mat4_transpose(window->chart->perspective));
-  vec4 ray_eye = mat4_mult_vec4(mat4_transpose(window->chart->perspective), ray_clip);
+  vec4 ray_eye = mat4_mult_vec4(mat4_inv(window->chart->perspective), ray_clip);
   ray_eye.z = 1.0f;
   ray_eye.w = 0.0f;
-  printf("ray_eye %f %f %f %f\n", ray_eye.x, ray_eye.y, ray_eye.z, ray_eye.w);
   // Step 4: 4d World Coordinates
-  vec4 ray_world = mat4_mult_vec4(mat4_transpose(window->chart->look), ray_eye);
-  printf("ray_world %f %f %f %f\n", ray_world.x, ray_world.y, ray_world.z, ray_world.w);
+  vec4 ray_world = mat4_mult_vec4(mat4_inv(window->chart->look), ray_eye);
 
   // Map coords to point on xy plane
-  vec2 xy_point = line_plane_collision(Vec3(0, 0, 1), Vec3(0, 0, 0), ray_world.xyz, cam->eye);
-  xy_point.x *= -1;
+  vec3 xyz_point = line_plane_collision(Vec3(0, 0, 1), Vec3(0, 0, 0), ray_world.xyz, cam->eye);
+  xyz_point.x *= -1;
 
-  return xy_point;
+  return xyz_point;
 }
 
 void axes_update(Window* window) {
   bool button1_last = window->mouse_last[GLFW_MOUSE_BUTTON_1];
   bool button1_cur = window->mouse_cur[GLFW_MOUSE_BUTTON_1];
   if (button1_last != button1_cur) {
-    vec2 xy_point = world_coords_xy(window, &window->chart->cam);
-    printf("xy_point %f %f\n", xy_point.x, xy_point.y);
-    if (xy_point.x != 0.0f && xy_point.y != 0.0f) {
-      if (!sel_started && button1_cur == GLFW_PRESS) {
-        sel_started = true;
-        sel_start = xy_point;
-      } else if (button1_cur == GLFW_RELEASE) {
-        sel_ended = true;
-        sel_end = xy_point;
-      }
+    vec3 xyz_point = world_coords_xyz(window, &window->chart->cam);
+    if (!selecting && button1_cur == GLFW_PRESS) {
+      selecting = true;
+      sel_start = xyz_point.xy;
+    } else if (button1_cur == GLFW_RELEASE) {
+      sel_reset();
+      sel_write_vertices();
     }
   }
-  if (sel_started && sel_ended) {
-    printf("sel_reset()\n");
-    sel_reset();
+  if (selecting) {
+    vec3 xyz_point = world_coords_xyz(window, &window->chart->cam);
+    sel_end = xyz_point.xy;
+    sel_write_vertices();
   }
 }
 
--- jeditrader/linalg.h
+++ jeditrader/linalg.h
@@ -660,3 +660,43 @@ static void mat4_print(mat4 mat) {
   }
   printf("\n");
 }
+
+// TODO: make fast and pretty
+static inline mat4 mat4_inv(mat4 m) {
+  mat4 ret;
+
+  float n11 = m.data[0][0], n12 = m.data[1][0], n13 = m.data[2][0], n14 = m.data[3][0];
+  float n21 = m.data[0][1], n22 = m.data[1][1], n23 = m.data[2][1], n24 = m.data[3][1];
+  float n31 = m.data[0][2], n32 = m.data[1][2], n33 = m.data[2][2], n34 = m.data[3][2];
+  float n41 = m.data[0][3], n42 = m.data[1][3], n43 = m.data[2][3], n44 = m.data[3][3];
+
+  float t11 = n23 * n34 * n42 - n24 * n33 * n42 + n24 * n32 * n43 - n22 * n34 * n43 - n23 * n32 * n44 + n22 * n33 * n44;
+  float t12 = n14 * n33 * n42 - n13 * n34 * n42 - n14 * n32 * n43 + n12 * n34 * n43 + n13 * n32 * n44 - n12 * n33 * n44;
+  float t13 = n13 * n24 * n42 - n14 * n23 * n42 + n14 * n22 * n43 - n12 * n24 * n43 - n13 * n22 * n44 + n12 * n23 * n44;
+  float t14 = n14 * n23 * n32 - n13 * n24 * n32 - n14 * n22 * n33 + n12 * n24 * n33 + n13 * n22 * n34 - n12 * n23 * n34;
+
+  float det = n11 * t11 + n21 * t12 + n31 * t13 + n41 * t14;
+  float idet = 1.0f / det;
+
+  ret.data[0][0] = t11 * idet;
+  ret.data[0][1] = (n24 * n33 * n41 - n23 * n34 * n41 - n24 * n31 * n43 + n21 * n34 * n43 + n23 * n31 * n44 - n21 * n33 * n44) * idet;
+  ret.data[0][2] = (n22 * n34 * n41 - n24 * n32 * n41 + n24 * n31 * n42 - n21 * n34 * n42 - n22 * n31 * n44 + n21 * n32 * n44) * idet;
+  ret.data[0][3] = (n23 * n32 * n41 - n22 * n33 * n41 - n23 * n31 * n42 + n21 * n33 * n42 + n22 * n31 * n43 - n21 * n32 * n43) * idet;
+
+  ret.data[1][0] = t12 * idet;
+  ret.data[1][1] = (n13 * n34 * n41 - n14 * n33 * n41 + n14 * n31 * n43 - n11 * n34 * n43 - n13 * n31 * n44 + n11 * n33 * n44) * idet;
+  ret.data[1][2] = (n14 * n32 * n41 - n12 * n34 * n41 - n14 * n31 * n42 + n11 * n34 * n42 + n12 * n31 * n44 - n11 * n32 * n44) * idet;
+  ret.data[1][3] = (n12 * n33 * n41 - n13 * n32 * n41 + n13 * n31 * n42 - n11 * n33 * n42 - n12 * n31 * n43 + n11 * n32 * n43) * idet;
+
+  ret.data[2][0] = t13 * idet;
+  ret.data[2][1] = (n14 * n23 * n41 - n13 * n24 * n41 - n14 * n21 * n43 + n11 * n24 * n43 + n13 * n21 * n44 - n11 * n23 * n44) * idet;
+  ret.data[2][2] = (n12 * n24 * n41 - n14 * n22 * n41 + n14 * n21 * n42 - n11 * n24 * n42 - n12 * n21 * n44 + n11 * n22 * n44) * idet;
+  ret.data[2][3] = (n13 * n22 * n41 - n12 * n23 * n41 - n13 * n21 * n42 + n11 * n23 * n42 + n12 * n21 * n43 - n11 * n22 * n43) * idet;
+
+  ret.data[3][0] = t14 * idet;
+  ret.data[3][1] = (n13 * n24 * n31 - n14 * n23 * n31 + n14 * n21 * n33 - n11 * n24 * n33 - n13 * n21 * n34 + n11 * n23 * n34) * idet;
+  ret.data[3][2] = (n14 * n22 * n31 - n12 * n24 * n31 - n14 * n21 * n32 + n11 * n24 * n32 + n12 * n21 * n34 - n11 * n22 * n34) * idet;
+  ret.data[3][3] = (n12 * n23 * n31 - n13 * n22 * n31 + n13 * n21 * n32 - n11 * n23 * n32 - n12 * n21 * n33 + n11 * n22 * n33) * idet;
+
+  return ret;
+}
